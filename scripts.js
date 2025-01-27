2283819b18b44a2dc191b35ed78c449494b44f49
maibutnie <56802951+maibutnie@users.noreply.github.com>
2025-01-27 15:56:32 +0100

    easy resume 2025

---
+++ scripts.js
@@ -0,0 +1,117 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+var points = [],
+    velocity2 = 5,
+    canvas = 
+document.getElementById('container'),
+  context = canvas.getContext('2d'),
+  radius = 5,
+  boundaryX = 200,
+  boundaryY = 200,
+  numberOfPoints = 30;
+
+init();
+
+function init() {
+  for (var i = 0; i<numberOfPoints; i++) {
+    createPoint();
+  }
+  for (var i = 0, l=points.length; i<l; i++) {
+    var point = points[i];
+    if(i == 0) {
+      points[i].buddy = points[points.length-1];
+    } else {
+      points[i].buddy = points[i-1];
+    }
+  }
+  
+  animate();
+}
+
+function createPoint() {
+  var point = {}, vx2, vy2;
+  point.x = Math.random()*boundaryX;
+  point.y = Math.random()*boundaryY;
+  point.vx = (Math.floor(Math.random())*2-1)*Math.random();
+  vx2 = Math.pow(point.vx, 2);
+  vy2 = velocity2 - vx2;
+  point.vy = Math.sqrt(vy2) * (Math.random()*2-1);
+  points.push(point);
+}
+
+function resetVelocity(point, axis, dir) {
+  if(axis == 'x') {
+    point.vx = dir*Math.random();  
+    vx2 = Math.pow(point.vx, 2);
+  vy2 = velocity2 - vx2;
+  point.vy = Math.sqrt(vy2) * (Math.random()*2-1);
+  } else {
+    point.vy = dir*Math.random();  
+    vy2 = Math.pow(point.vy, 2);
+  vx2 = velocity2 - vy2;
+  point.vx = Math.sqrt(vx2) * (Math.random()*2-1);
+  }
+}
+
+function drawCircle(x, y) {
+  context.beginPath();
+  context.arc(x, y, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = '#AF1B3F';
+  context.fill();  
+}
+
+function drawLine(x1, y1, x2, y2) {
+  context.beginPath();
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.strokeStyle = '#473144'
+  context.stroke();
+}  
+
+function draw() {
+  for(var i =0, l=points.length; i<l; i++) {
+    var point = points[i];
+    point.x += point.vx;
+    point.y += point.vy;
+    drawCircle(point.x, point.y);
+    drawLine(point.x, point.y, point.buddy.x, point.buddy.y);
+    if(point.x < 0+radius) {
+      resetVelocity(point, 'x', 1);
+    } else if(point.x > boundaryX-radius) {
+      resetVelocity(point, 'x', -1);
+    } else if(point.y < 0+radius) {
+      resetVelocity(point, 'y', 1);
+    } else if(point.y > boundaryY-radius) {
+      resetVelocity(point, 'y', -1);
+    } 
+  }
+}
+
+function animate() {
+  context.clearRect ( 0 , 0 , 200 , 200 );
+  draw();
+  requestAnimationFrame(animate);
+}
